3582adc2d8bf32869840b3b823a084529d33043d
kimswiftrn <2RNSwift@gmail.com>
2019-12-04 13:47:44 -0800

    I updated this with more loops

---
+++ aboutme.js
@@ -0,0 +1,51 @@
+// use strict;
+
+console.log('this js file is connected');
+
+// string: some characters(letters, spaces, numbers) strung together
+// quotes make a string
+// variables let us save information by giving that information a name
+// var variableName = 'value for that variable';
+      
+      
+
+var westSEattle = prompt('Do your patients have central IV lines?');
+if(hasIV.toLowerCase()==='yes'){
+alert('We can help');
+console.log(central)
+}
+ else{
+  alert('Ok. Keep us in mind if you need us. ');
+  console.log(nocentral)
+}
+
+
+   //var nurseTrain = prompt('Do your nurses need yearly training?');
+//if(nurseTrain.toLowerCase()==='yes'){
+// alert('We can help with that');
+// console.log(needstrain);
+
+// } else{
+//   alert('Ok. Keep us in mind if you need us.');
+ //  console.log(notrain);
+ //}
+
+ // var saveMoney = prompt('Would yo like us to come out and show you how you can save money by training with us?');
+//if (saveMoney.toLowerCase()==='yes'){
+//  alert ('Patients are the first priority');
+//  console.log(savemoney);
+  
+ // } else{
+ //        alert('Ok. Keep us in mind if you need us. ');
+ //        console.log(dontsavemoney);
+ // }
+//     var visitTomorrow= prompt('Should I come tomorrow?');
+//if(visitTomorrow.toLowerCase()==='yes')
+ //    alert('See you tomorrow');
+ //    console.log(visittom);
+    
+//     else {
+//       alert('Ok. Keep us in mind if you need us. ');
+//       console.log(novisit);
+       
+    }
---
+++ class3loops.js
@@ -0,0 +1,47 @@
+'use strict';
+
+console.log('js linked!');
+
+//practicing loops: countdouwn!
+
+var timeToLiftoff = 10:
+
+//console.log a countadown from 10 to 1, then liftoff!
+
+console.log(timeToLiftoff); // should log 10
+timetoLiftoff = timeToLiftoff -1
+console.log(timeToLiftoff); // should log 9
+timeToLiftoff = timeToLiftoff -1;
+
+// keep doing that 8 more times
+//wound be a side piece of code. 
+
+// instead, let's use a loop!
+//loop should stop when timeToLiftoff is 0
+//so loop should keep going while timeToLiftoff is more than 0
+
+while(while timeToLiftoff >0){
+  console.log(timeToLiftoff);
+  timeToLiftoff = timeToLiftoff - 1;
+}
+console.log("liftoff")
+// now on to arrays... she will use the square bracket...this is a while loop. 
+var fruits = [ '''apple, banana, catalopur, dragonfruit'];
+console.log (fruits[0]);
+fruits[0]= 'apricot';
+console.log(fruits[0]);
+var i =0;
+while 9i< fruits.length){
+  console.log(fruits [i]);
+  i=i+1;
+  }
+
+  for(var i=0; i<fruits.length;i =i +1)
+  {
+    console.log(fruits[i]);
+}
+// save this to remember what she said.
+while(/*something, come back to this*?){
+  console.log(timeToLiftoff);
+  timeToLiftoff = timeToLiftoff - 1;
+}
